0996756df63277532578f228afc94e56acd9de9c
Bo Liu <bo.robotics@gmail.com>
2019-12-22 14:36:31 -0500

    [WIP] add rectified RS_T265

--- donkeycar/templates/myconfig.py
+++ donkeycar/templates/myconfig.py
@@ -15,15 +15,18 @@ IMAGE_H = 224
 
 # #RealSense CAMERA
 #DRIVE_LOOP_HZ = 15
-#CAMERA_TYPE = "RS_D435i"   # (RS_D435i|RS_T265|PICAM|WEBCAM|CVCAM|CSIC|V4L|MOCK)
-#IMAGE_W = 640 
-#IMAGE_H = 480 
+CAMERA_TYPE = "RS_T265_StereoRectified"   # (RS_D435i|RS_T265|RS_T265_StereoRectified|PICAM|WEBCAM|CVCAM|CSIC|V4L|MOCK)
+IMAGE_W = 240
+IMAGE_H = 140
+RS_FOV=120 
 #RS_FRAME_RATE = DRIVE_LOOP_HZ
 #RS_IMG_TYPE = "color"
 #USE_RS_IMU = True
 
 # PWM driver
 PCA9685_I2C_BUSNUM = 1 
+
+AUTO_RECORD_ON_THROTTLE = False #if true, we will record whenever throttle is not zero. if false, you must manually toggle recording with some other trigger. Usually circle button on joystick.  
 
 # #STEERING - Dakar
 STEERING_CHANNEL = 0            #channel on the 9685 pwm board 0-15
@@ -36,7 +39,7 @@ THROTTLE_FORWARD_PWM = 480 #pwm value for max forward throttle
 THROTTLE_STOPPED_PWM = 390      #pwm value for no movement
 THROTTLE_REVERSE_PWM = 300 #pwm value for max reverse throttle
 
-DEFAULT_MODEL_TYPE = 'motion'   #(linear|categorical|rnn|imu|motion|behavior|3d|localizer|latent)
+DEFAULT_MODEL_TYPE = 'rnn'   #(linear|categorical|rnn|imu|motion|behavior|3d|localizer|latent)
 
 CONTROLLER_TYPE='xbox'               #(ps3|ps4|xbox|nimbus|wiiu|F710|rc3)# import os
 
