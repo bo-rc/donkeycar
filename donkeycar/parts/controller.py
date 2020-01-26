867158a519f54222434272d42ed19155d7033d59
Bo Liu <bo.robotics@gmail.com>
2020-01-26 15:15:40 -0500

    [xbox] change to two sticks driving

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -1127,8 +1127,8 @@ class XboxOneJoystickController(JoystickController):
         }
 
         self.axis_trigger_map = {
-            'right_stick_horz': self.set_steering,
-            'left_stick_vert': self.set_throttle,
+            'left_stick_horz' : self.set_steering,
+            'right_stick_vert' : self.set_throttle,
             # Forza Mode
             'right_trigger': self.magnitude(),
             'left_trigger': self.magnitude(reversed = True),
